5cbad2f130b1cfbe8fbacebfa5ba79491647e7fa
LoudSoftware <contact@loudsoftware.tk>
2018-03-05 15:17:48 -0500

    Finished model and Main class Currently working: Basic json to Object conversion!

--- Question1/src/main/java/Main.java
+++ Question1/src/main/java/Main.java
@@ -1,10 +1,44 @@
 import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import model.WadingPool; // Our wading pool model
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public Main() {
+    private final String filename;
+    private Gson gson;
+    private static final Type wadingPoolType = new TypeToken<List<WadingPool>>(){}.getType();
+    private List<WadingPool> data = new ArrayList<WadingPool>(100);
+
+    private Main(String filename) {
+        this.filename = filename;
+        this.gson = new Gson();
+        JsonReader reader;
+
+        // Try to read the file
+        try {
+            reader = new JsonReader(new FileReader(filename));
+            data = gson.fromJson(reader, wadingPoolType);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for (WadingPool pool :
+                data) {
+            System.out.println(pool);
+        }
     }
 
     public static void main(String[] args) {
-
+        if (args.length > 0){
+            new Main(args[0]);
+        } else {
+            new Main("wading-pools-filtered.json");
+        }
     }
 }
--- Question1/src/main/java/model/WadingPool.java
+++ Question1/src/main/java/model/WadingPool.java
@@ -1,4 +1,84 @@
 package model;
 
-public class WadingPool {
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WadingPool implements Comparable<WadingPool> {
+    // Node attributes
+    private WadingPool parent;
+    private WadingPool left;
+    private WadingPool right;
+
+    //Properties loaded from JSON
+    private String name;
+    private ArrayList<Double> coordinates;
+
+    // Getters
+    public WadingPool getParent() {
+        return parent;
+    }
+
+    public WadingPool getLeft() {
+        return left;
+    }
+
+    public WadingPool getRight() {
+        return right;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getCoordinates() {
+        return coordinates;
+    }
+
+    // Setters
+
+    public void setParent(WadingPool parent) {
+        this.parent = parent;
+    }
+
+    public void setLeft(WadingPool left) {
+        this.left = left;
+    }
+
+    public void setRight(WadingPool right) {
+        this.right = right;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCoordinates(ArrayList<Double> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    /**
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    public int compareTo(WadingPool o) {
+        //TODO add the distance algo to compare two nodes according to distance between each other
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public String toString() {
+        return "WadingPool{" +
+                "parent=" + parent +
+                ", left=" + left +
+                ", right=" + right +
+                ", name='" + name + '\'' +
+                ", coordinates=" + coordinates.toString() +
+                '}';
+    }
 }
