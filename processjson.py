6d229b6a01d0dbf32f5d0b910cec8a9d56c9ce96
LoudSoftware <contact@loudsoftware.tk>
2018-03-05 15:15:50 -0500

    Modified processjson.py to be more generic and added .pl file generation

--- processjson.py
+++ processjson.py
@@ -3,21 +3,41 @@ from pprint import pprint
 
 data = json.load(open('wading-pools.json'))
 
-def processToJSON():
-    filtered = {}
+generalData = []
 
+
+def processToGeneral():
     features = data['features']
 
     for item in features:
         coordinates = item['geometry']['coordinates']
         name = item['properties']['NAME']
+        pool = {
+            "name": name,
+            "coordinates": coordinates
+        }
+        generalData.append(pool)
 
-        filtered[name] = coordinates
+    pprint(generalData)
 
-    pprint(filtered)
 
+def processToJSON():
+    print('__Begin json conversion__')
     with open('wading-pools-filtered.json', 'w') as outfile:
-        json.dump(filtered, outfile)
+        json.dump(generalData,outfile,ensure_ascii=False)
+
+def processToProlog():
+    print('__Begin Prolog conversion__')
+    with open('wading-pools-filtered.pl','w') as outfile:
+        for item in generalData:
+            out = "Pool(" + '"' + item['name'] + '"' + "," + str(item['coordinates'][0]) + "," + str(item['coordinates'][1]) + ")." + "\n"
+            print(out)
+            outfile.write(out)
+        
+
+
 
 if __name__ == '__main__':
+    processToGeneral()
     processToJSON()
+    processToProlog()
